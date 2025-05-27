7cb759c505d0636dbb9a59234d8779c4a797b4bc
Ari Karim <arikarim146@gmail.com>
2025-05-27 14:51:04 +0300

    Add configuration management to Workflower module with logger support

--- lib/workflower.rb
+++ lib/workflower.rb
@@ -3,7 +3,25 @@
 require_relative "workflower/version"
 require "workflower/manager"
 require "workflower/acts_as_workflower"
+
 module Workflower
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+  end
+
+  class Configuration
+    attr_accessor :logger
+
+    def initialize
+      @logger = nil
+    end
+  end
 end
